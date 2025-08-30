ec000dbc42dc4e9a935740dfe722f4b00477ad72
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-30 10:26:04 +0100

    Create index.tsx

---
+++ mobile-development-setup/app-example/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+
+---
+
+## 📂 `app-example/app/(tabs)/index.tsx`
+
+```tsx
+import { Text, View } from "react-native";
+
+export default function TabOneScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text> First App Created </Text>
+    </View>
+  );
+}
