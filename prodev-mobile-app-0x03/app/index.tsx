549c19a2de29706da95f779dcc8b2a3c0fe3b5ca
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-31 23:27:19 +0100

    Update index.tsx

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -7,57 +7,57 @@ export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        {/* Navigation */}
         <View style={styles.navGroup}>
           <Ionicons name="arrow-back" size={25} />
-          <Image source={require("@/assets/images/logo.png")} />
+          <Image source={require('@/assets/images/logo.png')} />
         </View>
-
-        {/* Title */}
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
         <Text style={styles.smallText}>
           Enter your email and password to sign in.
         </Text>
 
-        {/* Form */}
         <View style={styles.formGroup}>
           <View>
             <Text style={styles.placeholderText}>Email</Text>
-            <TextInput
-              keyboardType="email-address"
-              style={styles.inputField}
-            />
+            <TextInput keyboardType="email-address" style={styles.inputField} />
           </View>
-
           <View style={{ marginTop: 20 }}>
             <Text style={styles.placeholderText}>Password</Text>
             <View style={styles.passwordGroup}>
-              <TextInput style={{ flex: 1 }} secureTextEntry />
+              <TextInput style={{ flex: 1 }} />
               <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
             </View>
           </View>
-
           <Text style={styles.forgotPasswordText}>Forgot password?</Text>
         </View>
 
-        {/* Sign In Button */}
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Sign in</Text>
         </TouchableOpacity>
 
-        {/* Divider */}
         <View style={styles.dividerGroup}>
-          <View style={styles.divider}></View>
-          <Text style={styles.dividerText}>OR</Text>
-          <View style={styles.divider}></View>
+          <View
+            style={styles.divider}
+          ></View>
+          <Text
+            style={styles.dividerText}
+          >
+            OR
+          </Text>
+          <View
+            style={styles.divider}
+          ></View>
         </View>
 
-        {/* Social Login */}
         <View style={styles.socialMediaButtonGroup}>
           <TouchableOpacity style={styles.socialMediaButton}>
             <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
             >
               <Image source={require("@/assets/images/google.png")} />
               <Text style={styles.socialMediaButtonText}>
@@ -68,7 +68,11 @@ export default function Index() {
 
           <TouchableOpacity style={styles.socialMediaButton}>
             <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
             >
               <Image source={require("@/assets/images/facebook.png")} />
               <Text style={styles.socialMediaButtonText}>
@@ -78,10 +82,9 @@ export default function Index() {
           </TouchableOpacity>
         </View>
 
-        {/* Footer */}
         <View style={styles.subTextGroup}>
           <Text style={styles.subText}>Don't have an account?</Text>
-          <Text style={styles.subTextJoin}> Join now</Text>
+          <Text style={styles.subTextJoin}>Join now</Text>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
