077c98a44b1a3cf7d51c632b5c8960d389ea3202
yum-yum03 <elbekaymariampro@gmail.com>
2025-08-31 23:07:31 +0100

    Update index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,11 +4,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-
       <View>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
+  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
