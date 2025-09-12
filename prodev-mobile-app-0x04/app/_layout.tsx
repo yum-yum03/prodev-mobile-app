38ecc24fc087bc211825fc83a2974e39c9b43db1
yum-yum03 <elbekaymariampro@gmail.com>
2025-09-12 21:14:43 +0100

    Create _layout.tsx

---
+++ prodev-mobile-app-0x04/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="join" />
+      <Stack.Screen name="signin" />
+    </Stack>
+  );
+}
