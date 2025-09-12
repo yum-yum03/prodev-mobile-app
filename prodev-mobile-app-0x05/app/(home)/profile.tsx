e1e61a30b3ccb0224ad9f1d72e7747c01cca8336
yum-yum03 <elbekaymariampro@gmail.com>
2025-09-12 21:59:08 +0100

    Create profile.tsx

---
+++ prodev-mobile-app-0x05/app/(home)/profile.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>👤 Profile Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  text: { fontSize: 20 },
+});
