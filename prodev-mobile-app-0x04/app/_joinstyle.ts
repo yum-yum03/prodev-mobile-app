7d6f06b0c545c1cc01e5388532a20d23be2875a0
yum-yum03 <elbekaymariampro@gmail.com>
2025-09-12 21:23:13 +0100

    Create _joinstyle.ts

---
+++ prodev-mobile-app-0x04/app/_joinstyle.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  input: {
+    width: "100%",
+    padding: 12,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: "#34967C",
+    padding: 14,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
