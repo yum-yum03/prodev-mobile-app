26bc37e8b1f2f829165f6d570cd93c9e86a553b7
yum-yum03 <elbekaymariampro@gmail.com>
2025-09-12 22:07:52 +0100

    Create _propertyCardStyle.ts

---
+++ prodev-mobile-app-0x05/styles/_propertyCardStyle.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    marginBottom: 10,
+    padding: 12,
+  },
+  image: { width: "100%", height: 140, borderRadius: 10 },
+});
